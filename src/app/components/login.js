0545ddd8e01ba5c4c84261c293e22765f742ea27
Александр <nikol.alex06@mail.ru>
2024-09-13 19:23:49 +0300

    Пофиксил уязвимость в регистрации и входе пользователей

--- src/app/components/login.js
+++ src/app/components/login.js
@@ -36,7 +36,7 @@ export default function Login ({close}){
 
     if (isRegUser) {
         return (
-            <div className="h-screen fixed w-full bg-main/70 backdrop-blur-xl pt-52 wrapper mx-auto px-5">
+            <div className="h-screen fixed w-full bg-main/70 backdrop-blur-xl pt-52 px-5">
                 <div className=" mx-auto max-w-[511px] p-7 max-[1146px]:p-5 max-[640px]:2.5 rounded-[10px] border-super-light border-2">
                     <p className="pb-5 text-2xl/[25px] max-[1146px]:text-lg/[25px] max-[640px]:text-base/[18px] tracking-[0.02em]">Вход в систему</p>
                     {regErr ? <p className="text-red-600 pb-5 text-lg/[25px] max-[1146px]:text-base/[25px] max-[640px]:text-xs/[18px]">Неверно указан логин или пароль</p> : <></>}
@@ -46,7 +46,7 @@ export default function Login ({close}){
                         formData = Object.fromEntries(formData);
                         sendFormData('http://localhost:5000/login', 'POST', formData)
                         }}>
-                        <input placeholder="Логин" name="login" className="px-7 bg-transparent py-2.5 rounded-[10px] border-super-light border-2 placeholder:font-serif palceholder:text-xl max-[1146px]:placeholder:text-base max-[640px]:placeholder:text-sm palceholder:font-normal"></input>
+                        <input placeholder="Адрес электронной почты" name="email" className="px-7 bg-transparent py-2.5 rounded-[10px] border-super-light border-2 placeholder:font-serif palceholder:text-xl max-[1146px]:placeholder:text-base max-[640px]:placeholder:text-sm palceholder:font-normal"></input>
                         <input placeholder="Пароль" name="password" type='password' className="px-7 bg-transparent py-2.5 rounded-[10px] border-super-light border-2 placeholder:font-serif palceholder:text-xl palceholder:font-normal"></input>
                         <div className="flex flex-nowrap flex-row max-[360px]:flex-col justify-center gap-5 max-[640px]:gap-3 items-center">
                             <button type="submit" className="px-5 max-[1146px]:px-3.5 py-2.5 max-[1146px]:py-[6px] rounded-[10px] font-serif text-xl max-[1146px]:text-base max-[640px]:text-sm bg-medium w-auto max-[360px]:w-full">Войти</button>
@@ -74,7 +74,6 @@ export default function Login ({close}){
                         
                     }}>
                         <input placeholder="Никнейм" name="name" className="px-7 bg-transparent py-2.5 rounded-[10px] border-super-light border-2 placeholder:font-serif palceholder:text-xl palceholder:font-normal"></input>
-                        <input placeholder="Логин" name="login" className="px-7 bg-transparent py-2.5 rounded-[10px] border-super-light border-2 placeholder:font-serif palceholder:text-xl palceholder:font-normal"></input>
                         <input placeholder="Email" name="email" className="px-7 bg-transparent py-2.5 rounded-[10px] border-super-light border-2 placeholder:font-serif palceholder:text-xl palceholder:font-normal"></input>
                         {isCorrectPass ? <></> : <p className="text-red-600 text-lg/[25px] max-[1146px]:text-base/[25px] max-[640px]:text-xs/[18px]">Введенный и повторенный пароли не совпадают</p>}
                         <input placeholder="Пароль" name="password" type='password' className="px-7 bg-transparent py-2.5 rounded-[10px] border-super-light border-2 placeholder:font-serif palceholder:text-xl palceholder:font-normal"></input>
