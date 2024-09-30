9c4b76b8f9083c2d76cddb7ef50aaece3fe4b1fd
Александр <nikol.alex06@mail.ru>
2024-09-30 21:15:56 +0300

    Добавление панели админа

--- src/app/[...taskid]/page.js
+++ src/app/[...taskid]/page.js
@@ -7,13 +7,15 @@ export default async function Page({params}) {
     let url, answerUrl;
     let id = params.taskid[0];
 
-    await fetch(`http://localhost:5000/answer?id=${id}`, {method: 'GET'})
-            .then(res => res.json()).then(data => answerUrl = data.url);
-
-    await fetch(`http://localhost:5000/task?id=${id}`, {method: 'GET'})
-            .then(res => res.json())
-            .then(data => url = data.url);
-
+    if (!isNaN(id)){
+        await fetch(`http://localhost:5000/answer?id=${id}`, {method: 'GET'})
+                .then(res => res.json()).then(data => answerUrl = data.url);
+    
+        await fetch(`http://localhost:5000/task?id=${id}`, {method: 'GET'})
+                .then(res => res.json())
+                .then(data => url = data.url);
+    }
+    
     return (
         <div className="wrapper mx-auto flex flex-col gap-y-[30px] max-[700px]:gap-y-5 px-0 max-[1280px]:px-5">
             <InfoPart id={id} params={params} url={url}/>
