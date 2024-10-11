5c02e54d9e41aa1d4e69f5a29a5da5689a1a5f62
Александр <nikol.alex06@mail.ru>
2024-10-11 15:50:37 +0300

    feat: Add two function; TODO: create disign for some parts in account control

--- src/app/admin_panel/page.js
+++ src/app/admin_panel/page.js
@@ -79,7 +79,7 @@ function AddUser () {
                     <Image alt='arrow' src={'/Arrow.svg'} height={43} width={43}/>
                 </button>
             </div>
-            <form className={`flex flex-col gap-y-5 px-7 ${isOpen ? 'h-auto' : 'h-0 overflow-hidden'} items-end`} id={'add-user-form'} onSubmit={function (e)  {
+            <form className={`flex-col gap-y-5 px-7 ${isOpen ? 'h-auto flex' : 'h-0 overflow-hidden none'} items-end`} id={'add-user-form'} onSubmit={function (e)  {
                     e.preventDefault();
                     let formData = new FormData(document.getElementById('add-user-form'));
                     formData = Object.fromEntries(formData);
@@ -132,9 +132,9 @@ function RemoveUser () {
                     <Image alt='arrow' src={'/Arrow.svg'} height={43} width={43}/>
                 </button>
             </div>
-            <from>
             <form className={`flex-col gap-y-5 px-7 ${isOpen ? 'h-auto flex' : 'h-0 overflow-hidden none'} items-end`} id={'remove-user-form'} onSubmit={function (e)  {
                     e.preventDefault();
+                    console.log(e.nativeEvent.submitter.type);
                     let formData = new FormData(document.getElementById('remove-user-form'));
                     formData = Object.fromEntries(formData);
                     sendFormData(`http://${host}:5000/removeuser`, formData)
@@ -146,7 +146,6 @@ function RemoveUser () {
                 </div>
                 <SubmitButton color={'bright'} text={'Удалить'}/>
             </form>
-            </from>
         </div>
     )
 }
@@ -154,26 +153,100 @@ function RemoveUser () {
 function TakeAdminRights () {
     const [isOpen, setIsOpen] = useState(false);
 
+    function sendFormData (url, formData) {
+        let json = JSON.stringify(formData);
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: json,
+            cache: 'no-cache'
+        })
+        .then((res) => res.json())
+        .then((data) => { 
+            if (data.res == 'success'){
+                alert('Запрос выполнен успешно')
+            } else {
+                alert('Запрос не выполнен')
+            }
+        })
+        .catch((err) => {
+            alert('Ошибка запроса');
+            console.log(err);
+        })
+    }
+
     return (
         <div className='flex flex-col gap-y-5'>
             <div className='flex justify-between items-center'>
                 <p>Предоставление прав админа</p>
-                <button>
+                <button onClick={() => setIsOpen(!isOpen)} className={`${isOpen ? 'rotate-180' : 'rotate-0'} transition-all duration-200`}>
                     <Image alt='arrow' src={'/Arrow.svg'} height={43} width={43}/>
                 </button>
             </div>
+            <form className={`flex-col gap-y-5 px-7 ${isOpen ? 'h-auto flex' : 'h-0 overflow-hidden none'} items-end`} id={'admin-user-form'} onSubmit={function (e)  {
+                    e.preventDefault();
+                    let formData = new FormData(document.getElementById('admin-user-form'));
+                    formData = Object.fromEntries(formData);
+                    if (e.nativeEvent.submitter.id == 'take') {
+                        sendFormData(`http://${host}:5000/takerights`, formData);
+                    } else {
+                        sendFormData(`http://${host}:5000/recallrights`, formData);
+                    }
+                    
+                }}>
+                <div className='w-full flex gap-x-5 items-center max-w-[752.33px]'>
+                    <input placeholder={'User ID'} name={'userid'} className={'w-full px-5 py-2.5 rounded-[10px] bg-transparent border-super-light border-2'}></input>
+                    <span className='w-fit'>or</span>
+                    <input placeholder={'Email'} name={'email'} className={'w-full px-5 py-2.5 rounded-[10px] bg-transparent border-super-light border-2'}></input>
+                </div>
+                <div className="flex gap-x-5">
+                    <SubmitButton color={'bright'} id={'take'} text={'Предоставить'}/>
+                    <SubmitButton color={'medium'} id={'recall'} text={'Отозвать'}/>
+                </div>
+            </form>
         </div>
     )
 }
 
 function ShowAllUsers () {
-    const [isOpen, setIsOpen] = useState(false);
+    const [tableData, setTableData] = useState([]);
+
+    async function showData () {
+        await fetch(`http://${host}:5000/getusers`, {method: 'GET'})
+            .then(res => res.json())
+            .then(data => setTableData(data));
+    }
 
     return (
         <div className='flex flex-col gap-y-5'>
             <div className='flex justify-between items-center'>
-                <button className='w-full text-left'>Показать всех пользователей</button>
+                <button className='w-full text-left' onClick={() => showData()}>Показать всех пользователей</button>
             </div>
+            <ul className={'flex flex-col divide-y-[1px] divide-medium'}>
+                <li className="grid grid-cols-6 py-3">
+                    <p className="justify-self-center">userId</p>
+                    <p className="justify-self-center">isAdmin</p>
+                    <p className="justify-self-center">donetask_ids</p>
+                    <p className="justify-self-center">name</p>
+                    <p className="justify-self-center">password</p>
+                    <p className="justify-self-center">email</p>
+                </li>
+                {tableData.map(row => {
+                    return (
+                        <li className="grid grid-cols-6 py-3 items-center" key={row.userId}>
+                            <p className="justify-self-center">{row.userId}</p>
+                            <p className="justify-self-center">{`${row.isAdmin == '1'}`}</p>
+                            <p className="justify-self-center">{row.donetask_ids}</p>
+                            <p className="justify-self-center">{row.name}</p>
+                            <p className="justify-self-center">{row.password}</p>
+                            <p className="justify-self-center">{row.email}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
